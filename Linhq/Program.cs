4f79e11c1d8cfd487e2435341439e21066cdedf2
NigmaMrlk <bk20100420@gmail.com>
2023-09-18 13:26:20 +0700

    no message

---
+++ Linhq/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linhq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "A", "A", "A", "A", "B", "B", "B", "C", "D" };
+
+            var duplicates = list.GroupBy(x => x)
+                       .Where(g => g.Count() == 1)
+                       .Select(y => new {y.Key})
+                       .ToList();
+
+            foreach (var i in duplicates)
+            {
+                Console.WriteLine(i.Key);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
